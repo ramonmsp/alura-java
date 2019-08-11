cebb43cd3aae537e961558cd746ed3581086a267
Ramon Maciel <msp.ramon@gmail.com>
2019-08-10 21:06:07 -0300

    commit inicial - até polimorfismo

---
+++ bytebank-herdado/src/Gerente.java
@@ -0,0 +1,25 @@
+//Gerente herda atributos de funcionário
+public class Gerente extends Funcionario{
+
+	private int senha;
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	public int getSenha() {
+		return senha;
+	}
+	
+	//reescrita na classe que herda. Mesma assinatura de método	
+	public double getBonificacao(){
+		return super.getBonificacao() + super.getSalario();
+	}	
+}
---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,20 @@
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		Funcionario ramon = new Funcionario();
+		ramon.setNome("Ramon Maciel");
+		ramon.setCpf("123.123.123.12");
+		ramon.setSalario(2598.00);
+		
+		System.out.println(ramon.getNome());
+		System.out.println(ramon.getBonificacao());
+		
+		
+		
+		
+		
+
+	}
+
+}
